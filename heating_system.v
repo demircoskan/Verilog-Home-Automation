cd63cc2ad2c4bc8c35e97ea28fbe27d5730c06ed
Demir <demir.coskan@std.yeditepe.edu.tr>
2024-11-08 03:00:57 +0300

    Create heating_system.v

---
+++ heating_system.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.11.2024 02:28:28
+// Design Name: 
+// Module Name: heating_system
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module heating_system(
+input [`temperature_sensor_data_width-1:0] temp,
+input [`motion_sensor_data_width-1:0] presence,
+input [`window_sensor_data_width-1:0] window,
+input [`ac_cool_data_width-1:0] heating_system,            
+output heating_system
+    );
+    wire low_temp = (temp < temp_treshold);
+    assign heating_system = ~ac_cool & low_temp & presence & ~window;
+endmodule
