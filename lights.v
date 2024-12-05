e66571001db4f620e20a9d50f986b3e6adcda3e1
Demir <demir.coskan@std.yeditepe.edu.tr>
2024-12-06 00:47:05 +0300

    Update lights.v

--- lights.v
+++ lights.v
@@ -25,6 +25,17 @@ input [`daylight_sensor_data_width-1:0] daylight,
 input [`motion_sensor_data_width-1:0] presence,
 output lights
     );
+    //Data Flow Modelling
     wire low_light = (daylight < light_threshold);
     assign lights = low_light & presence;
+endmodule
+    
+    //Behavioral Modelling
+always @(*) begin
+        if (daylight < `light_threshold && presence) begin
+            lights = 1;
+        end else begin
+            lights = 0;
+        end
+    end   
 endmodule
